2799ca1c297902595103d714e51d2cf8eb1530f1
Mirza Krak <mirza.krak@northern.tech>
2018-09-03 06:42:02 +0000

    meta-mender-sunxi: fix typo in boot.cmd
    fdt_addr should be ftd_addr_r.
    
    Signed-off-by: Mirza Krak <mirza.krak@northern.tech>

--- meta-mender-sunxi/recipes-bsp/u-boot/files/aarch64/boot.cmd
+++ meta-mender-sunxi/recipes-bsp/u-boot/files/aarch64/boot.cmd
@@ -3,5 +3,5 @@ setenv bootargs console=${console} console=tty1 root=${mender_kernel_root} rootw
 mmc dev ${mender_uboot_dev};
 load ${mender_uboot_root} ${fdt_addr_r} /boot/${fdtfile};
 load ${mender_uboot_root} ${kernel_addr_r} /boot/Image;
-booti ${kernel_addr_r} - ${fdt_addr};
+booti ${kernel_addr_r} - ${fdt_addr_r};
 run mender_try_to_recover;
