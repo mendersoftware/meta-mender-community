ba58818fe8150318645b961cd8c852c77de2e92f
Josef Holzmayr <jester@theyoctojester.info>
2023-07-23 22:22:57 +0200

    fix: Tegra, Jetpack 5: remove deprecated test scripts
    Changelog: Title
    Ticket: None
    
    Signed-off-by: Josef Holzmayr <jester@theyoctojester.info>

--- meta-mender-tegra/meta-mender-tegra-jetpack5/scripts/test/setup_image_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-scriptdir="$(dirname "$(readlink -f "$0")")"
-imagesdir=$(realpath ${scriptdir}/../../../../../build/tmp/deploy/images)
-echo "Serving images from ${imagesdir}"
-pushd ${imagesdir}
-python -m SimpleHTTPServer 8080 
