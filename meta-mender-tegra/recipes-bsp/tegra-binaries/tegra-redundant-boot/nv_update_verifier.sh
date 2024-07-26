7ff577be5526543e80de131a2864da5d2d58a5de
Zachary T Welch <zach@aquabyte.ai>
2024-07-26 13:30:10 -0700

    nv_update_verifier.sh: fix string equality test
    Changelog: Title
    Issue: None
    
    Signed-off-by: Zachary T Welch <zach@aquabyte.ai>

--- meta-mender-tegra/recipes-bsp/tegra-binaries/tegra-redundant-boot/nv_update_verifier.sh
+++ meta-mender-tegra/recipes-bsp/tegra-binaries/tegra-redundant-boot/nv_update_verifier.sh
@@ -4,6 +4,6 @@ upgrade_available=$(/sbin/fw_printenv upgrade_available | cut -d "=" -f2)
 
 # Only verify booted slot if we are not in an upgrade process
 # since mender will do that on commit (through fw_setenv)
-if [ "$upgrade_available" == "0" ]; then
+if [ "$upgrade_available" = "0" ]; then
 	/usr/sbin/nvbootctrl verify
 fi
